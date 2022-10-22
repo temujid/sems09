c269ad84925e25278fb0d6328c74badc26ce2fe5
Timur <temuchin@ya.ru>
2022-10-22 11:38:53 +0400

    all changes

--- task_68/Program.cs
+++ task_68/Program.cs
@@ -1,24 +1,35 @@
-﻿/* Задача 64: Задайте значения N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
- N = 5. -> "5, 4, 3, 2, 1"
- N = 8. -> "8, 7, 6, 5, 4, 3, 2, 1"
- */
- 
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int count = 1;
-NaturalToLow(number, count);
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
 
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
 
-void NaturalToLow(int n, int count)
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m, n);
+
+Console.WriteLine();
+
+void AkkermanFunction(int m, int n)
 {
-    if (count > n)
+    Console.Write(Akkerman(m, n));
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
     {
-        return;
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
     }
     else
     {
-        NaturalToLow(n, count + 1);
-        Console.Write(count + " ");
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
     }
-Console.WriteLine();
 }
